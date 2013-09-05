505ea8fe7a2bbdd0130989e3e85ef2cba4f8f903
BurtonHank <hankburton92@hotmail.com>
2013-09-05 02:54:42 -0700

    Preliminary layout.
    Buttons are arbitrary at this point. No functionality is in place yet.

--- RepoWorkspace/PinochleScore/gen/com/burton_chandler/pinochlescore/R.java
+++ RepoWorkspace/PinochleScore/gen/com/burton_chandler/pinochlescore/R.java
@@ -24,14 +24,32 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080007;
-        public static final int listView1=0x7f080000;
-        public static final int textView1=0x7f080001;
-        public static final int textView2=0x7f080002;
-        public static final int textView3=0x7f080003;
-        public static final int textView4=0x7f080004;
-        public static final int textView5=0x7f080005;
-        public static final int textView6=0x7f080006;
+        public static final int RelativeLayout1=0x7f080000;
+        public static final int TableLayout1=0x7f080001;
+        public static final int TableLayout2=0x7f080012;
+        public static final int action_settings=0x7f080019;
+        public static final int button1=0x7f080014;
+        public static final int button2=0x7f080015;
+        public static final int button3=0x7f080017;
+        public static final int button4=0x7f080018;
+        public static final int tableRow1=0x7f080006;
+        public static final int tableRow2=0x7f08000a;
+        public static final int tableRow3=0x7f08000e;
+        public static final int tableRow4=0x7f080002;
+        public static final int tableRow5=0x7f080013;
+        public static final int tableRow6=0x7f080016;
+        public static final int textView1=0x7f080007;
+        public static final int textView10=0x7f080004;
+        public static final int textView11=0x7f080005;
+        public static final int textView12=0x7f080003;
+        public static final int textView2=0x7f080008;
+        public static final int textView3=0x7f08000b;
+        public static final int textView4=0x7f08000c;
+        public static final int textView5=0x7f08000f;
+        public static final int textView6=0x7f080010;
+        public static final int textView7=0x7f080009;
+        public static final int textView8=0x7f08000d;
+        public static final int textView9=0x7f080011;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
