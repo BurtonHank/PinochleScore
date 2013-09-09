78b9db79097fbcefcdfa754a732999f566f16f42
BurtonHank <hankburton92@hotmail.com>
2013-09-08 19:19:24 -0700

    COMMITTING RIGHT MEOW

--- RepoWorkspace/PinochleScore/gen/com/burton_chandler/pinochlescore/R.java
+++ RepoWorkspace/PinochleScore/gen/com/burton_chandler/pinochlescore/R.java
@@ -27,11 +27,14 @@ public final class R {
         public static final int RelativeLayout1=0x7f080000;
         public static final int TableLayout1=0x7f080001;
         public static final int TableLayout2=0x7f080012;
-        public static final int action_settings=0x7f080019;
-        public static final int button1=0x7f080014;
+        public static final int action_settings=0x7f08001c;
         public static final int button2=0x7f080015;
         public static final int button3=0x7f080017;
         public static final int button4=0x7f080018;
+        public static final int editText1=0x7f080019;
+        public static final int editText2=0x7f08001a;
+        public static final int meldbutton=0x7f080014;
+        public static final int savemeld=0x7f08001b;
         public static final int tableRow1=0x7f080006;
         public static final int tableRow2=0x7f08000a;
         public static final int tableRow3=0x7f08000e;
@@ -54,6 +57,7 @@ public final class R {
     public static final class layout {
         public static final int activity_main=0x7f030000;
         public static final int score_row_layout=0x7f030001;
+        public static final int set_meld_layout=0x7f030002;
     }
     public static final class menu {
         public static final int main=0x7f070000;
--- RepoWorkspace/PinochleScore/src/com/burton_chandler/pinochlescore/MainActivity.java
+++ RepoWorkspace/PinochleScore/src/com/burton_chandler/pinochlescore/MainActivity.java
@@ -2,7 +2,12 @@ package com.burton_chandler.pinochlescore;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.view.LayoutInflater;
 import android.view.Menu;
+import android.view.View;
+import android.view.ViewGroup.LayoutParams;
+import android.widget.Button;
+import android.widget.PopupWindow;
 
 public class MainActivity extends Activity {
 
@@ -10,6 +15,28 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        final Button btnSetMeld = (Button)findViewById(R.id.meldbutton);
+        btnSetMeld.setOnClickListener(new Button.OnClickListener(){
+        	public void onClick(View arg0) {
+        		LayoutInflater layoutInflater = (LayoutInflater)getBaseContext().getSystemService(LAYOUT_INFLATER_SERVICE);
+        		View setMeldView = layoutInflater.inflate(R.layout.set_meld_layout, null);
+        		final PopupWindow meldPopup = new PopupWindow(
+        				setMeldView,
+        				LayoutParams.WRAP_CONTENT,
+        				LayoutParams.WRAP_CONTENT);
+        		
+        		Button btnSave = (Button)setMeldView.findViewById(R.id.savemeld);
+        		btnSave.setOnClickListener(new Button.OnClickListener()
+        		{
+        			public void onClick(View v){
+        				meldPopup.dismiss();
+        			}
+        		});
+        		
+        		meldPopup.showAtLocation(btnSetMeld,1, 50, -75);
+        	}
+        });
     }
 
 
